a956c7f88c4ce603d0165737b37e37bc0bf17051
sandy <alakaythelionn@gmail.com>
2020-02-25 11:09:21 -0600

    the code that controles the media has now been improved

--- assets/index.js
+++ assets/index.js
@@ -1,22 +1,8 @@
+import MediaPlayer from './MediaPlayer.js';
+
 const video = document.querySelector("video");
-const button = document.querySelector("button");
-
-function MediaPlayer(config){
-    this.media = config.the;
-}
-
-MediaPlayer.prototype.play = function(){
-    this.media.play();
-};
-
-MediaPlayer.prototype.pause = function () {
-    this.media.pause();
-};
 
 const player = new MediaPlayer({the: video});
-button.onclick = () => {
-    if (player.paused)
-        player.play();
-    else
-        player.pause();
-}
+
+const button = document.querySelector('playButton');
+button.onclick = () => player.togglePlay();
