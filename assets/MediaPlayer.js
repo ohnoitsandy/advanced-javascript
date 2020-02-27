826ff57f88c14a4e05d1a10bd1a1c059b85866dd
sandy <alakaythelionn@gmail.com>
2020-02-27 16:10:57 -0600

    Some progress has been made

--- assets/MediaPlayer.js
+++ assets/MediaPlayer.js
@@ -1,6 +1,15 @@
 function MediaPlayer(config){
     this.media = config.the;
+    this.plugins = config.plugins || [];
+
+    this._initPlugins();
 }
+
+MediaPlayer.prototype._initPlugins = function () {
+    this.plugins.forEach(plugin => {
+        plugin.run();
+    })
+};
 
 MediaPlayer.prototype.play = function(){
     this.media.play();
@@ -10,11 +19,23 @@ MediaPlayer.prototype.pause = function () {
     this.media.pause();
 };
 
+MediaPlayer.prototype.mute = function () {
+   this.media.muted = true;
+};
+
+MediaPlayer.prototype.unmute = function () {
+    this.media.muted = false;
+};
+
 MediaPlayer.prototype.togglePlay = function (){
     if (this.media.paused)
         this.play();
     else
         this.pause();
+};
+
+MediaPlayer.prototype.toggleSound = function () {
+this.media.mute ? this.unmute() : this.mute();
 };
 
 export default MediaPlayer;
--- assets/index.js
+++ assets/index.js
@@ -1,8 +1,11 @@
 import MediaPlayer from './MediaPlayer.js';
-
+import AutoPlay from './plugins/AutoPlay.js';
 const video = document.querySelector("video");
 
-const player = new MediaPlayer({the: video});
+const player = new MediaPlayer({the: video, plugins: [new AutoPlay()]});
 
-const button = document.querySelector('playButton');
-button.onclick = () => player.togglePlay();
+const togglePlay = document.querySelector('btnTogglePlay');
+togglePlay.onclick = () => player.togglePlay();
+
+const toggleSound = document.querySelector('btnToggleSound');
+toggleSound.onclick = () => player.toggleSound();
